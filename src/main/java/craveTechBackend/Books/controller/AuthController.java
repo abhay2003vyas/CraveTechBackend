f1f5ae056121d129367bd48b696c173d71f7e4af
Abhay Vyas <vyas0994@gmail.com>
2026-02-09 19:50:40 +0530

    update api in backend

--- src/main/java/craveTechBackend/Books/controller/AuthController.java
+++ src/main/java/craveTechBackend/Books/controller/AuthController.java
@@ -1,7 +1,6 @@
 package craveTechBackend.Books.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.*;
 
 import craveTechBackend.Books.models.User;
@@ -11,25 +10,32 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(
+        origins = {
+                "http://localhost:5173",
+                "https://cravetech.vercel.app"
+        },
+        allowedHeaders = "*",
+        methods = {
+                RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.OPTIONS
+        }
+)
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
-
     @PostMapping("/register")
     public String register(@RequestBody User user) {
-
         return authService.register(user);
     }
 
-
     @PostMapping("/login")
-    public Map<String, String> login(
-            @RequestBody User user
-    ) {
-
+    public Map<String, String> login(@RequestBody User user) {
         return authService.login(user);
     }
 }
