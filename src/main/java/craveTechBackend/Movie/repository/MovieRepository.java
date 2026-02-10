e466f4fa15eaf8755040305ce6f8bc588028a66e
Abhay Vyas <vyas0994@gmail.com>
2026-02-11 01:49:22 +0530

    update repository

--- src/main/java/craveTechBackend/Movie/repository/MovieRepository.java
+++ src/main/java/craveTechBackend/Movie/repository/MovieRepository.java
@@ -1,7 +1,13 @@
 package craveTechBackend.Movie.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import craveTechBackend.Movie.models.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    // 👤 Movies created by a specific user (Explore page)
+    List<Movie> findByCreatedBy(String createdBy);
 }
