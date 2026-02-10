4769c65c8ddbc05d527122159d63dee6c5b7557f
Abhay Vyas <vyas0994@gmail.com>
2026-02-11 01:24:31 +0530

    update api

--- src/main/java/craveTechBackend/Movie/security/SecurityConfig.java
+++ src/main/java/craveTechBackend/Movie/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
             "http://localhost:3000",
-            "https://cravetech-frontend.onrender.com"
+            "https://cravetech.vercel.app"
         ));
 
         config.setAllowedMethods(List.of(
